27d71b22da00067e6374499f27580afddba14ee8
kunal_s <128156983+kunal-suroshe@users.noreply.github.com>
2023-07-16 12:58:00 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            ViewBag.Celsius = celsius;
+            ViewBag.Fahrenheit = fahrenheit;
+
+            return View();
+        }
+    }
+}
